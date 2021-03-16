1d0f7b55e8a077ec3aeb72ea8b873d823b1dcc69
CoderSiddharthYadav <siddhu.969488@gmail.com>
2021-03-16 12:56:20 -0700

    first commit

---
+++ bulb.js
@@ -0,0 +1,15 @@
+
+
+    function bulb(){
+        var img=document.getElementsByTagName("img");
+        for(var i=0;i<img.length;i++)
+        img[i].src="on.png"
+        
+        }
+        function bulb1(){
+        var img=document.getElementsByTagName("img");
+        for(var i=0;i<img.length;i++)
+        img[i].src="of.png"
+        
+        }
+    
